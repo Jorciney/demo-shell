7bb0e9a9262f49ce4e1d0b05e5160b1bed5eee17
Jorciney <jorciney.dias@hotmail.com>
2024-03-19 09:28:58 +0100

    Adding module federation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () => loadRemoteModule({
+      type: 'manifest', // Angular >= 13
+      remoteName: 'demo-mfe',
+      exposedModule: './Component'
+    })
+      .then(esm => {
+        console.log('esm:', esm);
+        return esm['AppComponent'];
+      })
+  }
+];

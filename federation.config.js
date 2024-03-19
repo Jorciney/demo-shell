f0c21f337fc639be85d0f98cbd46294371009089
Laurens Marcelis <laurens.marcelis@telenetgroup.be>
2024-03-19 15:58:57 +0100

    correct config

---
+++ federation.config.js
@@ -0,0 +1,15 @@
+const { withNativeFederation, share } = require('@angular-architects/native-federation/config');
+
+module.exports = withNativeFederation({
+
+  shared: share({
+    '@angular/core': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/common': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/common/http': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/router': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    '@angular/platform-browser': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    'rxjs': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+    'tslib': { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+  }),
+
+});
